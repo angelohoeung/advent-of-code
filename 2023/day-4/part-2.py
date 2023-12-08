6c4560593a11e8d751738b6d82b475946a5051cf
AngeloHoeung <angelohoeung@gmail.com>
2023-12-08 17:50:01 -0500

    day 4 part 2 scuffed solution but it works

--- 2023/day-4/part-2.py
+++ 2023/day-4/part-2.py
@@ -4,14 +4,18 @@ import re
 with open(f"{dirname(__file__)}/input.txt", "r") as file:
     lines = file.read().splitlines()
 
-total = 0
+num_matches = []
 
 for line in lines:
     line = re.sub(r'Card +\d+: ', '', line)
     cards = line.split(' | ')
     cards[0] = list(cards[0].split())
     cards[1] = list(cards[1].split())
-    matches = sum(nums in cards[0] for nums in cards[1])
-    total += 2 ** (matches - 1) if matches > 0 else 0
+    num_matches.append(sum(nums in cards[0] for nums in cards[1]))
 
-print(total)
+instances = [1 for i in num_matches]
+for i, num in enumerate(num_matches):
+    for j in range(i + 1, i + num + 1):
+        instances[j] += instances[i]
+
+print(sum(instances))
